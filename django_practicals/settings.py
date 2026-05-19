9bd2004aca8783ae46ee1318cc4b39fbc7f5ec8c
EBIN BABU <ebin.babu.in@icloud.com>
2026-05-19 17:29:23 +0530

    virtual enviornments updated

--- django_practicals/settings.py
+++ django_practicals/settings.py
@@ -79,15 +79,10 @@ WSGI_APPLICATION = 'django_practicals.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': 'localhost',  # or your DB server IP
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
